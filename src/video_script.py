37a570c96c7482893336bfa222606b46a7ee2d24
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2023-04-14 17:14:42 +0800

    Test

--- src/video_script.py
+++ src/video_script.py
@@ -16,7 +16,7 @@ def video_feed():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('camera.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
