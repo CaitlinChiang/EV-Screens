c91403a1931e554ba860d511b84090ac5c7e497a
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2023-04-18 21:49:54 +0800

    test

---
+++ src/scripts/headlights-script.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(12, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+
+while True:
+    headlight_indicator = GPIO.input(12)
+    light_indicator = GPIO.input(16)
+
+    if headlight_indicator == True:
+        GPIO.output(20, GPIO.HIGH)
+    else:
+        GPIO.output(20, GPIO.LOW)
+
+    if light_indicator == True:
+        GPIO.output(21, GPIO.HIGH)
+    else:
+        GPIO.output(21, GPIO.LOW)
+
+    time.sleep(0.1)
